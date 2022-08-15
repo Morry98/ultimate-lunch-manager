775bb7edf733bad883d572f2a82c50a2d1c76ef5
Morry <morandomatteo98@gmail.com>
2022-08-15 17:39:32 +0200

    implement restaurants model

---
+++ ultimate_lunch_manager/model/restaurants.py
@@ -0,0 +1,14 @@
+import datetime
+
+from sqlalchemy import Column, String, Integer, DateTime
+
+from ultimate_lunch_manager.database import Base
+
+
+class Restaurants(Base):
+    __tablename__ = "restaurants"
+
+    id_restaurant = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    priority = Column(Integer, nullable=True)
+    creation_date_utc = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
