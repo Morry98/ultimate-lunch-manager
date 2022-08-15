327516e038f96baf1e5d81c448cb9aeddded4f24
Morry <morandomatteo98@gmail.com>
2022-08-15 17:38:04 +0200

    implement possible_times model

---
+++ ultimate_lunch_manager/model/possible_times.py
@@ -0,0 +1,14 @@
+import datetime
+
+from sqlalchemy import Column, String, Integer, DateTime
+
+from ultimate_lunch_manager.database import Base
+
+
+class PossibleTimes(Base):
+    __tablename__ = "possible_times"
+
+    id_possible_time = Column(Integer, primary_key=True, index=True)
+    time = Column(String, nullable=False)
+    priority = Column(Integer, nullable=True)
+    creation_date_utc = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
