6cc1ad3390b0d9002bbc3ac5642b45673fb10818
Morry <morandomatteo98@gmail.com>
2022-10-02 15:18:08 +0200

    database.py add get db function

--- ultimate_lunch_manager/database.py
+++ ultimate_lunch_manager/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 from ultimate_lunch_manager.config.config import Config
 
@@ -10,3 +10,12 @@ engine = create_engine(config.DB_CONN_STR)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 Base = declarative_base()
+
+
+# Dependency
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
