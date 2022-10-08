08d1cefb57ac4e151d5ad0cfc201bf5806c5f535
Morry <morandomatteo98@gmail.com>
2022-10-08 21:24:11 +0200

    implement manage_cache_thread

---
+++ ultimate_lunch_manager/utils/manage_cache_thread.py
@@ -0,0 +1,35 @@
+from threading import Thread
+from time import sleep
+from typing import List, Callable
+
+from ultimate_lunch_manager.possible_times import possible_times_service
+from ultimate_lunch_manager.users import users_service
+
+CACHED_FUNCTION: List[Callable] = [
+    possible_times_service.get_time_all_options,
+    possible_times_service.get_possible_times,
+    users_service.get_users,
+]
+
+
+class ManageCacheThread(Thread):
+    active = False
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run(self) -> None:
+        while self.active:
+            for function in CACHED_FUNCTION:
+                function()
+            sleep(1)
+
+    def stop(self) -> None:
+        self.active = False
+
+    def start(self) -> None:
+        self.active = True
+        self.run()
+
+    def is_running(self) -> bool:
+        return self.active
