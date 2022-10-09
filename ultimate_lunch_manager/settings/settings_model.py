54beee4b33446c02987cf9bd69e8c374583be046
Morry <morandomatteo98@gmail.com>
2022-10-09 21:53:22 +0200

    settings model and schema update channel_id and client type

--- ultimate_lunch_manager/settings/settings_model.py
+++ ultimate_lunch_manager/settings/settings_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, CLOB
 
 from ultimate_lunch_manager.database import Base
 
@@ -9,8 +9,8 @@ class Settings(Base):
     id_settings = Column(Integer, primary_key=True, index=True)
     participants_notification_time = Column(String, nullable=True)
     lunch_notification_time = Column(String, nullable=True)
-    client = Column(String, nullable=True)
-    channel_id = Column(String, nullable=True)
+    client = Column(CLOB, nullable=True)
+    channel_id = Column(Integer, nullable=True)
     channel_name = Column(String, nullable=True)
     participants_notification_timezone = Column(String, nullable=True)
     compute_lunch_timezone = Column(String, nullable=True)
--- ultimate_lunch_manager/settings/settings_schema.py
+++ ultimate_lunch_manager/settings/settings_schema.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel
 class SettingsBase(BaseModel):
     participants_notification_time: Optional[str] = None
     lunch_notification_time: Optional[str] = None
-    client: Optional[str] = None
-    channel_id: Optional[str] = None
+    client: Optional[bytes] = None
+    channel_id: Optional[int] = None
     channel_name: Optional[str] = None
     participants_notification_timezone: Optional[str] = None
     compute_lunch_timezone: Optional[str] = None
